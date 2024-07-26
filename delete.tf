7178a078adf02cb3d3bf424ba870ee7951aaec30
MohitTah <mohit.s@edts.ca>
2024-07-26 10:25:46 -0700

    a

--- delete.tf
+++ delete.tf
@@ -5,7 +5,6 @@ provider "aws" {
 # Define the bucket you want to keep
 resource "aws_s3_bucket" "keep_bucket" {
   bucket = "disapproval-page-bucket-pos"
-bucket = "my-unique-bucket-name128434521544312"
 }
 
 
@@ -20,8 +19,8 @@ resource "aws_s3_bucket" "delete_bucket_2" {
 }
 
 
-resource "aws_s3_bucket" "delete_bucket_2" {
+resource "aws_s3_bucket" "delete_bucket_3" {
   bucket = "elasticbeanstalk-ca-central-1-047753125980"
 }
 
-# Add similar resources for other buckets you want to delete
+
